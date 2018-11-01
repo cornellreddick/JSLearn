c1395076804302686f2485f8e23177c973ffefbe
Cornell Reddick <cornellreddick@outlook.com>
2018-11-01 14:15:41 -0400

    created switch statement.

---
+++ FirstConsoleAppInJS/FirstConsoleAppInJS/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+
+let medal = 'first place';
+
+switch (medal) {
+    case 'first place':
+        console.log('The champ');
+        break;
+    case 'second place':
+        console.log('You all most made it!');
+        break;
+    case 'third place':
+        console.log('You suck!')
+        break;
+   
+}
+//
