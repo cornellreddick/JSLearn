518912771eb4a826583f37f6f9f7f12155a5f7cb
Cornell Reddick <cornellreddick@outlook.com>
2018-11-12 13:08:27 -0500

    Using the Carousel Example from booth strap.

---
+++ FirstConsoleAppInJS/ChooseRightIterator.js
@@ -0,0 +1,16 @@
+// JavaScript source code\
+
+const cities = ['Orlando', 'Dubai', 'Edinburgh', 'Chennai', 'Accra',
+    'Denver', 'Eskisehir', 'Medellin', 'Yokohama'];
+
+const nums = [1, 50, 75, 200, 350, 525, 1000];
+
+//forEach Method iterate through all element in the Array.
+cities.forEach(city => console.log('Have you visited ' + city + '?'));
+
+//
+const longCities = cities.filter(city => city.length > 7);
+
+console.log(longCities);
+
+sin.get();
